a3e0364bd6993100ceb76d426313d70bc8b5bc0b
Hly1238 <ngxuannhu1@gmail.com>
2023-12-18 21:46:14 +0700

    'Dashboard-1/3'

--- BookTicketMovie/app/src/main/java/com/example/bookticketmovie/ui/home/HomeFragment.java
+++ BookTicketMovie/app/src/main/java/com/example/bookticketmovie/ui/home/HomeFragment.java
@@ -1,20 +1,52 @@
 package com.example.bookticketmovie.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.bookticketmovie.Adapter.FilmListAdapter;
+import com.example.bookticketmovie.Adapter.SliderAdapters;
+import com.example.bookticketmovie.Domain.ListFilm;
+import com.example.bookticketmovie.Domain.SliderItems;
+import com.example.bookticketmovie.R;
 import com.example.bookticketmovie.databinding.FragmentHomeBinding;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    //Recycler
+    private RecyclerView.Adapter adapterBestMovies, AdapterUpComing, adaterCategory;
+    private RecyclerView recyclerViewBestMovies, recyclerViewUpComing, recyclerViewCategory;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest, mStringRequest2, mStringRequest3;
+    private ProgressBar loading1, loading2, loading3;
+
     private FragmentHomeBinding binding;
+    private ViewPager2 viewPager2;
+    private Handler slideHandler = new Handler();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +58,94 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        //Slider
+        viewPager2 = binding.viewpagerSlider;
+
+        recyclerViewBestMovies = binding.recyclerView2;
+        recyclerViewBestMovies.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        loading1 = binding.progressBar;
+        banners();
+
+        sendRequest();
+
         return root;
+    }
+
+    private void sendRequest() {
+        mRequestQueue = Volley.newRequestQueue(requireContext());
+        loading1.setVisibility(View.VISIBLE);
+        mStringRequest=new StringRequest(Request.Method.GET, "https://moviesapi.ir/api/v1/movies?page=1", new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Gson gson = new Gson();
+                loading1.setVisibility(View.GONE);
+                ListFilm items = gson.fromJson(response,ListFilm.class);
+                adapterBestMovies = new FilmListAdapter(items);
+                recyclerViewBestMovies.setAdapter((adapterBestMovies));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loading1.setVisibility(View.GONE);
+                Log.i("UiLover", "OnError"+error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
+    }
+
+    private void banners(){
+        List<SliderItems> sliderItems = new ArrayList<>();
+        sliderItems.add(new SliderItems(R.drawable.wide1));
+        sliderItems.add(new SliderItems(R.drawable.wide));
+        sliderItems.add(new SliderItems(R.drawable.wide3));
+
+        viewPager2.setAdapter(new SliderAdapters(sliderItems, viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_ALWAYS);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1-Math.abs(position);
+                page.setScaleY(0.85f+r*0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.setCurrentItem(1);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                slideHandler.removeCallbacks(sliderRunnable);
+            }
+        });
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+        }
+    };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        slideHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        slideHandler.postDelayed(sliderRunnable, 2000);
     }
 
     @Override
