dee5e540fd55cdd095aa577817e7a6ef916d84ac
Hly1238 <ngxuannhu1@gmail.com>
2023-12-19 14:38:54 +0700

    'dashboard-1/3'

---
+++ AppBookTicketMovie/app/src/main/java/com/example/appbookticketmovie/Domain/Genre.java
@@ -0,0 +1,19 @@
+
+package com.example.appbookticketmovie.Domain;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Genre {
+    private List<GenreItem> genres;
+
+    public List<GenreItem> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenreItem> genres) {
+        this.genres = genres;
+    }
+}
